f27485b689a212fbb6529bf481c76a9eb722d2aa
MattIPv4 <me@mattcowley.co.uk>
2022-12-17 00:37:29 +0000

    Add speedtest via Docker to home automation

--- bootstrap-pi.sh
+++ bootstrap-pi.sh
@@ -2,14 +2,10 @@
 
 # Raspberry Pi OS 64-bit base, using Raspberry Pi Imager (enable SSH and set password in settings)
 # Once imaged, cd to the boot drive and run `sed -i '' "s/rootwait/rootwait ip=<ip>/" cmdline.txt` to set a static ip
+# Before running this script, run `sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y autoremove`, then `sudo reboot`
 # To run this script: `sudo apt-get -y install git && git clone https://github.com/MattIPv4/home-automation && ./home-automation/bootstrap-pi.sh`
 
 set -e -o pipefail
-
-# Update OS
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y autoremove
 
 # Install firewall
 sudo apt-get -y install ufw
@@ -30,14 +26,17 @@ sudo apt-get -y install libsdl2-2.0-0 libsdl2-mixer-2.0-0
 # Install divoom-control dependencies
 sudo apt-get -y install build-essential libbluetooth-dev
 
-# Clone PyDMXControl repo
-git clone https://github.com/MattIPv4/PyDMXControl ~/home-automation/lighting/PyDMXControl
-
 # Install FNM
 curl -fsSL https://fnm.vercel.app/install | bash
 sed -i "s/fnm env\`/fnm env --use-on-cd\`/" ~/.bashrc
 # shellcheck disable=SC1090
 source ~/.bashrc
+
+# Install Docker
+curl -fsSL https://get.docker.com | sudo sh
+
+# Clone PyDMXControl repo
+git clone https://github.com/MattIPv4/PyDMXControl ~/home-automation/lighting/PyDMXControl
 
 # Clone divoom-control and install
 git clone https://github.com/MattIPv4/divoom-control ~/home-automation/divoom-control
@@ -53,4 +52,12 @@ sudo systemctl enable "$PWD/lighting.service"
 sudo systemctl start lighting.service
 sudo systemctl status lighting.service
 sudo ufw allow 8080
+cd ~/
+
+# Enable and expose home automation speedtest
+cd ~/home-automation/speedtest
+sudo systemctl enable "$PWD/speedtest.service"
+sudo systemctl start speedtest.service
+sudo systemctl status speedtest.service
+sudo ufw allow 8081
 cd ~/
