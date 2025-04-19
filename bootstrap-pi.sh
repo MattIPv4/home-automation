f7c49d535bc5693c0be9e208039b7c01352a26f7
MattIPv4 <me@mattcowley.co.uk>
2025-04-19 22:33:01 +0100

    Remove lighting service

--- bootstrap-pi.sh
+++ bootstrap-pi.sh
@@ -17,42 +17,8 @@ sudo ufw default deny
 echo "y" | sudo ufw enable
 sudo ufw status verbose
 
-# Install Python
-sudo apt-get -y install python3 python3-pip python3-venv
-
-# Install PyDMXControl dependencies
-sudo apt-get -y install libsdl2-2.0-0 libsdl2-mixer-2.0-0
-
-# Install divoom-control dependencies
-sudo apt-get -y install build-essential libbluetooth-dev
-
-# Install FNM
-curl -fsSL https://fnm.vercel.app/install | bash
-sed -i "s/fnm env\`/fnm env --use-on-cd\`/" ~/.bashrc
-# shellcheck disable=SC1090
-source ~/.bashrc
-
 # Install Docker
 curl -fsSL https://get.docker.com | sudo sh
-
-# Clone PyDMXControl repo
-git clone https://github.com/MattIPv4/PyDMXControl ~/home-automation/lighting/PyDMXControl
-
-# Clone divoom-control and install
-git clone https://github.com/MattIPv4/divoom-control ~/home-automation/divoom-control
-cd ~/home-automation/divoom-control
-fnm use --install-if-missing
-npm ci
-npm link
-cd ~/
-
-# Enable and expose home automation lighting
-# cd ~/home-automation/lighting
-# sudo systemctl enable "$PWD/lighting.service"
-# sudo systemctl start lighting.service
-# sudo systemctl status lighting.service
-sudo ufw allow 8000
-# cd ~/
 
 # Enable and expose home automation speedtest
 cd ~/home-automation/speedtest
