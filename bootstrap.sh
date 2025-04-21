1f1926387552cabc91f6040363441ba9a92fcd55
MattIPv4 <me@mattcowley.co.uk>
2025-04-21 17:45:50 +0100

    Support Jetson Nano for bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Raspberry Pi:
+#   Raspberry Pi OS 64-bit Lite (no desktop), using Raspberry Pi Imager (enable SSH and set password in settings)
+#   Once imaged, cd to the boot drive and run `sed -i '' "s/rootwait/rootwait ip=<ip>/" cmdline.txt` to set a static ip
+#   Before running this script, run `sudo rpi-update`, then `sudo reboot`
+#
+# Jetson Nano:
+#   Before installing Armbian, the NVIDIA-provided image may need to be installed first (https://www.nvidia.com/jetsonnano-start/)
+#   Armbian Minimal for Jetson Nano, using Etcher (SSH enabled by default as root:1234)
+#   Archive image using kernel 6.1 appears to work: https://archive.armbian.com (jetson-nano/archive/Armbian_23.8.1_Jetson-nano_bookworm_current_6.1.50_minimal.img.xz)
+#   Before running this script, run `apt install -y avahi-daemon libnss-mdns && sed -i 's/publish-workstation=no/publish-workstation=yes/' /etc/avahi/avahi-daemon.conf`, then `sudo reboot`
+#
+# Before running this script, if required `sudo hostnamectl hostname <hostname>`, run `sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y autoremove`, then `sudo reboot`
+# To run this script: `sudo apt-get -y install git && git clone https://github.com/MattIPv4/home-automation /opt/home-automation && /opt/home-automation/bootstrap.sh`
+
+set -e -o pipefail
+
+# Install firewall
+sudo apt-get -y install ufw
+
+# Configure firewall
+echo "y" | sudo ufw reset
+sudo ufw allow 22
+sudo ufw default deny
+echo "y" | sudo ufw enable
+sudo ufw status verbose
+
+# Install Docker
+curl -fsSL https://get.docker.com | sudo sh
+
+# Enable and expose home automation speedtest
+sudo systemctl enable /opt/home-automation/speedtest/speedtest.service
+sudo systemctl start speedtest.service
+sudo systemctl status speedtest.service
+sudo ufw allow 8081
+
+# Enable and expose home automation smokeping
+sudo systemctl enable /opt/home-automation/smokeping/smokeping.service
+sudo systemctl start smokeping.service
+sudo systemctl status smokeping.service
+sudo ufw allow 8082
